d6ec5438894c0fe8645279062e5f0687a54fdefc
jmpantoja <jmpantoja@gmail.com>
2023-06-28 18:09:40 +0200

    parece que si

--- api/src/Auth/Domain/Model/RoleList.php
+++ api/src/Auth/Domain/Model/RoleList.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Auth\Domain\Model;
 
-use App\Auth\Domain\Model\VO\Role;
 use PlanB\DS\Attribute\ElementType;
 use PlanB\DS\Sequence\Sequence;
 
--- api/src/Auth/Domain/Model/User.php
+++ api/src/Auth/Domain/Model/User.php
@@ -82,7 +82,6 @@ class User implements Entity, UserInterface, PasswordAuthenticatedUserInterface
         return $this->password;
     }
 
-
     public function eraseCredentials()
     {
     }
